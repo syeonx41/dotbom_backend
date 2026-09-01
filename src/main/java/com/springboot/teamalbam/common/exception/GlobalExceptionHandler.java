c040b3d335fd861b96c225fef916c9ea629443e2
SeungyeonWoo <seungyeon3287@gmail.com>
2026-09-01 19:02:27 +0900

    fix: 예외 핸들러에 로그 추가

--- src/main/java/com/springboot/teamalbam/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/teamalbam/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.springboot.teamalbam.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.FileNotFoundException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(FileNotFoundException.class)
@@ -21,8 +23,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleUnexpected(Exception ex) {
+        log.error("처리되지 않은 예외 발생", ex);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("서버 오류가 발생했습니다.");
     }
-
 }
